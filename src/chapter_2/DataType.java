6675e6649366411cb580eaed597790d994e7d96b
kyumin1227 <kyumin12271227@gmail.com>
2024-09-06 21:10:16 +0900

    Lesson: 자료형

---
+++ src/chapter_2/DataType.java
@@ -0,0 +1,18 @@
+package chapter_2;
+
+public class DataType {
+
+    public static void main(String[] args) {
+
+        byte bar = 127;
+        short foo = 32767;
+        int bao = 2147483647;
+        long hao = 9223372036854775807L;
+
+        for (char value = 'A'; value <= 'Z'; value++) {
+            System.out.println(value);
+        }
+
+    }
+
+}
