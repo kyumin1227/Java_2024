67243c67968dadedfc708f2c9b831dd8b347771b
kyumin1227 <kyumin12271227@gmail.com>
2025-04-30 15:49:46 +0900

    Lesson: OOP (interface)

---
+++ src/oop/interfaceTest/MainTest.java
@@ -0,0 +1,41 @@
+package oop.interfaceTest;
+
+interface KL {
+    public abstract void print();
+}
+
+class T {
+    KL obj;
+    T(KL obj) {
+        this.obj = obj;
+    }
+    void prt() {
+        obj.print();
+    }
+}
+
+class P implements KL {
+    public void print() {
+        System.out.println("P");
+    }
+}
+
+class Q implements KL {
+    public void print() {
+        System.out.println("Q");
+    }
+}
+
+class Z implements KL {
+    public void print() {
+        System.out.println("Z");
+    }
+}
+
+public class MainTest {
+    public static void main(String[] args) {
+        (new T(new P())).prt();
+        (new T(new Q())).prt();
+        (new T(new Z())).prt();
+    }
+}
